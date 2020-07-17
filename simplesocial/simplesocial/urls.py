8314b90aecdbeac25c4a49e04acf38fe63faa8a1
tanviredu <tanviredu2018@gmail.com>
2020-07-17 14:38:52 +0600

    added

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePage.as_view(), name="home"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
